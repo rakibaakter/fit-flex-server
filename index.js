5e27434a7234d7f1b2cf5c9f7e293457ffaa9464
rakibaakter <rakibaakter635@gmail.com>
2023-12-04 10:29:08 +0600

    get specific trainers data

--- index.js
+++ index.js
@@ -77,6 +77,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/trainers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await trainerCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
