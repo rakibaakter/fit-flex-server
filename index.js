351a7699f1d92367b9ad3d2734fd94b8e6cd9e90
rakibaakter <rakibaakter635@gmail.com>
2023-12-05 14:44:33 +0600

    get users

--- index.js
+++ index.js
@@ -98,6 +98,12 @@ async function run() {
     });
 
     //user related api
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
